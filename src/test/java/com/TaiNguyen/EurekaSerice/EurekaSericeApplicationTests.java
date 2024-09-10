cffae4499d38e6845f90423d41e19f656ccd59df
Nguyễn Văn Tài <94286745+TaiNguyen14122002@users.noreply.github.com>
2024-09-10 14:21:30 +0700

    EurekaService

---
+++ src/test/java/com/TaiNguyen/EurekaSerice/EurekaSericeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TaiNguyen.EurekaSerice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaSericeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
